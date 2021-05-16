7e171e35aa07c18e103188c3ad51e312c55b171a
Jérôme <jerome.minatchy011928@gmail.com>
2021-05-16 15:33:33 -0300

    a

--- Resolution.py
+++ Resolution.py
@@ -7,14 +7,14 @@ def chargez(var,liste):
         for j in range(9):
             if(liste[i][j]>0):
                 var[i][j].set_domain((liste[i][j], liste[i][j]))
-def lpex1(liste):
+def lpex1(liste,taille):
 
     c=1
     M=CpoModel("Sudoku")
-    GRNG = range(9)
-    
-    var = [[M.integer_var(min=1, max=9, name="x" + str(l*9+c)) for l in range(9)] for c in range(9)]
-    
+    GRNG = range(taille)
+
+    var = [[M.integer_var(min=1, max=taille, name="x" + str(l*taille+c)) for l in range(taille)] for c in range(taille)]
+
     # Add alldiff constraints for lines
     for l in GRNG:
         M.add(M.all_diff([var[l][c] for c in GRNG]))
@@ -23,23 +23,38 @@ def lpex1(liste):
     for c in GRNG:
         M.add(M.all_diff([var[l][c] for l in GRNG]))
 
+    divx=0
+    divy=0
+    for i in range(2,taille):
+        if(taille%i==0 and i*i==taille):
+            divx=i
+    divy=divx
+    if divx==0 and divy==0:
+        for i in range(2,taille):
+            if(taille%i==0 and divx==0):
+                divx=i
+        for i in range(2,taille):
+            if(taille%i==0 and (divx*i)==taille and divy==0):
+                divy=i
+    
+    
     # Add alldiff constraints for sub-squares
-    ssrng = range(0, 9, 3)
+    ssrng = range(0, taille, divy)
     for sl in ssrng:
-        for sc in ssrng:
-            M.add(M.all_diff([var[l][c] for l in range(sl, sl + 3) for c in range(sc, sc + 3)]))
+        for sc in range(0, taille, divx):
+            M.add(M.all_diff([var[l][c] for l in range(sl, sl + divy) for c in range(sc, sc + divx)]))
     chargez(var,liste)
     msol=M.solve(TimeLimit=10)
     sol=[[msol[var[l][c]] for c in GRNG] for l in GRNG]
-    for i in range(9):
+    for i in range(taille):
         print(sol[i])
-sudoku =[[1, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0]]    
-lpex1(sudoku)
+sudoku =[[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+lpex1(sudoku,10)
