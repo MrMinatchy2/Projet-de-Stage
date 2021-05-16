f04480d6d26348e8846420b36ff0c7e900e9df5b
jerome Minatchy <jerome.minatchy011928@gmail.com>
2021-05-16 10:08:42 -0400

    résolu  avec docplex

--- Resolution.py
+++ Resolution.py
@@ -2,22 +2,17 @@ import docplex
 from docplex.cp.model import CpoModel
 import sys
 
-def set 
+def chargez(var,liste):
+    for i in range(9):
+        for j in range(9):
+            if(liste[i][j]>0):
+                var[i][j].set_domain((liste[i][j], liste[i][j]))
 def lpex1(liste):
-    myInput =[[0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
     c=1
     M=CpoModel("Sudoku")
     GRNG = range(9)
-
+    
     var = [[M.integer_var(min=1, max=9, name="x" + str(l*9+c)) for l in range(9)] for c in range(9)]
     
     # Add alldiff constraints for lines
@@ -33,10 +28,18 @@ def lpex1(liste):
     for sl in ssrng:
         for sc in ssrng:
             M.add(M.all_diff([var[l][c] for l in range(sl, sl + 3) for c in range(sc, sc + 3)]))
-            
+    chargez(var,liste)
     msol=M.solve(TimeLimit=10)
     sol=[[msol[var[l][c]] for c in GRNG] for l in GRNG]
     for i in range(9):
         print(sol[i])
-    
-lpex1()
+sudoku =[[1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]]    
+lpex1(sudoku)
