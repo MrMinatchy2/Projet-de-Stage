85afd99aa63db8bab3426114b721f8666693c0f2
Jérôme <jerome.minatchy011928@gmail.com>
2021-06-02 03:28:56 -0300

    trop bvien

--- Interface_Graphique.py
+++ Interface_Graphique.py
@@ -5,7 +5,7 @@ import random
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFontMetrics
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLabel, QTextEdit
-Res=2
+Res=1
 class Text(QTextEdit):
     def __init__(self):
         QTextEdit.__init__(self)
@@ -274,7 +274,7 @@ def lpexl(liste,taille):
     sol=[[msol[var[l][c]] for c in GRNG] for l in GRNG]
     return sol
         
-fen = Fenetre(9)
+fen = Fenetre(10)
 
 l=[["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
 
